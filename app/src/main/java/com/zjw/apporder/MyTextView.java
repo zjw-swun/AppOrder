9fc9529155366211b147d43df2a8cabf131eeb92
zjw <819151780@qq.com>
2018-07-18 15:42:33 +0800

    Feature: recyclerView 局部刷新实践

---
+++ app/src/main/java/com/zjw/apporder/MyTextView.java
@@ -0,0 +1,29 @@
+package com.zjw.apporder;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Desc :
+ * date : 2018/7/6 下午3:45
+ *
+ * @author : jiawei
+ */
+@SuppressLint("AppCompatCustomView")
+public class MyTextView extends TextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
